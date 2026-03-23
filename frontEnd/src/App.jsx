8f544f7f2ef94d44be640f77bd4048e0753c3462
Bhanu329-210 <bhanuprakashalahari.04@gmail.com>
2026-03-23 18:37:21 +0530

    done

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Booking from './pages/Booking';
 import SeatBooking from './pages/SeatBooking';
