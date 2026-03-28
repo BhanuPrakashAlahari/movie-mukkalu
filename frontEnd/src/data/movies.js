625dbd3a4d1f7bb9c01917fa76d4659bce00db81
Bhanu329-210 <bhanuprakashalahari.04@gmail.com>
2026-03-28 22:46:09 +0530

    movies updated

--- frontEnd/src/data/movies.js
+++ frontEnd/src/data/movies.js
@@ -8,20 +8,19 @@ export const MOVIES_DATA = {
       status: 'Available',
       trailer: "https://youtu.be/t8HVxmysPC4?si=WtSwJtp8_c-V659T"
     },
-
     {
       name: "Arjun Reddy",
       poster: "https://res.cloudinary.com/diipfzmyj/image/upload/v1774412257/WhatsApp_Image_2026-03-24_at_03.31.47_t58oxx.jpg",
-      time: '3:40 PM',
-      slug: '03-40-PM-06-40-PM',
+      time: '1:00 PM',
+      slug: '01-00-PM-04-00-PM',
       status: 'Available',
       trailer: "https://www.youtube.com/watch?v=aozErj9NqeE"
     },
     {
       name: "Masoodha",
       poster: "https://res.cloudinary.com/diipfzmyj/image/upload/v1774716746/masoodha_l6xw95.jpg",
-      time: '1:00 PM',
-      slug: '01-00-PM-03-30-PM',
+      time: '4:00 PM',
+      slug: '04-00-PM-07-00-PM',
       status: 'Available',
       trailer: "https://youtu.be/5iwdHPO1EJk?si=cBxzFAVz4F3OsEjL"
     }
